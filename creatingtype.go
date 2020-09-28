0ff0d51d95f18290be641cd795eea612d8c3c60d
youlovestook <youlovestook@gmail.com>
2020-09-28 16:29:24 -0500

    more tutorials about type

---
+++ creatingtype.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// you can create your own type in GO
+var aa int
+
+type hotdog int
+
+var bb hotdog
+
+func main() {
+	aa = 42
+	bb = 43
+	fmt.Println(aa)
+	fmt.Printf("%T\n", aa)
+	fmt.Println(bb)
+	fmt.Printf("%T\n", bb)
+	aa = int(bb)
+	fmt.Println(aa)
+	fmt.Printf("%T\n", aa)
+}
