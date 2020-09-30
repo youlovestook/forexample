0468250dc8f38a9bd741c696dd2d0b7d9d6371f4
youlovestook <youlovestook@gmail.com>
2020-09-30 10:51:11 -0500

    nother one before the ninja test

--- mapdelete.go
+++ mapdelete.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 // delete(<map name>, "key")
+// if you delete a key that doesnt exist no error is thrown
 
 func main() {
 	m := map[string]int{
@@ -14,4 +15,8 @@ func main() {
 	delete(m, "moneypenny")
 
 	fmt.Println(m)
+
+	if v, ok := m["james"]; ok {
+		fmt.Println("james is in there", v)
+	}
 }
