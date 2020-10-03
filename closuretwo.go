ba53e930aa6685c53f87e87a5b60e7aaac904fcb
youlovestook <youlovestook@gmail.com>
2020-10-02 21:34:14 -0500

    more toots, hands on for functions next

--- closuretwo.go
+++ closuretwo.go
@@ -8,19 +8,16 @@ import "fmt"
 var juicy int // package level scope
 
 func main() {
-	foo()
-	fmt.Println(juicy)
-	{
-
-		yyz := 42
-		fmt.Println(yyz)
-	}
-
-	fmt.Println(yyz)
+	a := incrementor()
+	fmt.Println(a())
 
 }
 
-func foo() {
-	fmt.Println("hello")
-	juicy++
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+
+	}
 }
---
+++ recursion.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(4 * 3 * 2 * 1)
+	n := factorial(9)
+	fmt.Println(n)
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
