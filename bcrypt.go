6607eeee87615a4da4bb52a3d3c36a20445665cd
youlovestook <youlovestook@gmail.com>
2020-10-04 11:20:17 -0500

    level nine hands on

---
+++ bcrypt.go
@@ -0,0 +1,30 @@
+// bcrypt is a way to store password information
+// it encrypts it
+// encryption is jumbling up the information - you then compare the jumbled up with the new jumbled up
+// and that is a way to never store the password
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := `password123`
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+
+	loginPword1 := `password1234`
+
+	err = bcrypt.CompareHashAndPassword(bs, []byte(loginPword1))
+	if err != nil {
+		fmt.Println("YOU CAN'T LOGIN")
+		return
+	}
+
+	fmt.Println("You're logged in")
+}
---
+++ sortcustom.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	First string
+	Age   int
+}
+
+type ByName []Person //sort by name
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByName) Less(i, j int) bool { return a[i].First < a[j].First }
+
+type ByAge []Person //sort by age
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+func main() {
+	p1 := Person{"James", 32}
+	p2 := Person{"Moneypenny", 27}
+	p3 := Person{"Q", 64}
+	p4 := Person{"M", 56}
+
+	people := []Person{p1, p2, p3, p4}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+	fmt.Println("---------------")
+	sort.Sort(ByName(people))
+	fmt.Println(people)
+
+}
